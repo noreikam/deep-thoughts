d8ee976f67241561aa6394aa251a0e659cf3089f
noreikam <noreikam1@gmail.com>
2022-06-07 00:49:19 -0400

    first GraphQL query

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 const express = require('express');
+// Apollo server
+const { ApolloServer } = require('apollo-server-express');
+// typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+// create new Apollo server
+const server = new ApolloServer({
+  typeDefs, resolvers
+});
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+// Create new instance of Apollo server with GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate Apollo server with Express application as middleware
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where to test GraphQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// Call async function to start server
+startApolloServer(typeDefs, resolvers);
